a4c2fcd1cff118fc0217235a34562c80fa4dc6ce
Enzo Gebauer <enzo.gebauer@aln.senaicimatec.edu.br>
2022-10-30 22:51:51 -0300

    erro de body parser

--- Web/node/PROJETO_API_REST/src/servidor.js
+++ Web/node/PROJETO_API_REST/src/servidor.js
@@ -1,15 +1,22 @@
 const porta = 3003 
 const express = require('express')
 const app = express()
+const bancodedados = require('./bancoDeDados')
 app.get('/produtos',(req,res,next)=>{
-  console.log("middleware 1...")
-  next()
+  res.send(bancodedados.getProdutos())
 })
 
-app.get('/produtos',(req,res,next)=>{
-  res.send({nome:'notebook',preco:123.45}) //método send automaticamente converte o obj para JSON
+app.get('/produtos/:id',(req,res,next )=>{
+  res.send(bancodedados.getProduto(req.params.id))
+})
+app.post('/produtos ',(req,res,next) => { 
+  const produto = bancodedados.salvarProduto({
+    nome:req.body.name,
+    preco: req.body.preco
+  })
+  res.send(produto) //converte o obj em um json que será enviado para a web 
 })
 app.listen(porta,()=>{
-  console.log(`Servidor executando na porta ${porta}.`)
+  console.log(`Servidor executando na porta ${porta}.`) 
 })
  
