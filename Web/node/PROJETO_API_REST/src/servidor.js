08e8e9c096ac71eb9b395bf412e7c7e265451d4e
Enzo Gebauer <enzo.gebauer@aln.senaicimatec.edu.br>
2022-10-31 22:10:57 -0300

    continuação api rest

--- Web/node/PROJETO_API_REST/src/servidor.js
+++ Web/node/PROJETO_API_REST/src/servidor.js
@@ -2,6 +2,8 @@ const porta = 3003
 const express = require('express')
 const app = express()
 const bancodedados = require('./bancoDeDados')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true,}))
 app.get('/produtos',(req,res,next)=>{
   res.send(bancodedados.getProdutos())
 })
@@ -9,9 +11,17 @@ app.get('/produtos',(req,res,next)=>{
 app.get('/produtos/:id',(req,res,next )=>{
   res.send(bancodedados.getProduto(req.params.id))
 })
-app.post('/produtos ',(req,res,next) => { 
+app.post('/produtos/',(req,res,next) => { 
   const produto = bancodedados.salvarProduto({
-    nome:req.body.name,
+    nome:req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(produto) //converte o obj em um json que será enviado para a web 
+})
+app.post('/produtos/:id',(req,res,next) => { 
+  const produto = bancodedados.salvarProduto({
+    id:req.body.id,
+    nome:req.body.nome,
     preco: req.body.preco
   })
   res.send(produto) //converte o obj em um json que será enviado para a web 
